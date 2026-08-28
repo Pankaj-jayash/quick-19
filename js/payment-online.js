bea56b8f0e68bb67c0801a0eff769a146bfd5784
Pankaj-jayash <pankajking258@gmail.com>
2026-08-28 12:35:29 +0100

    Update payment-online.js

--- js/payment-online.js
+++ js/payment-online.js
@@ -13,7 +13,7 @@ class OnlinePaymentManager {
         this.merchantCode = 'QKDKAN';
         
         // API URL (Google Apps Script)
-        this.API_URL = 'https://script.google.com/macros/s/AKfycbwUaX6PZW3xpKwilMVEr_oXjFXKTMsz3qfUwVy8icPjQjY5i7e6hLTWHz4-0kwhZBM1aw/exec';
+        this.API_URL = 'https://script.google.com/macros/s/AKfycbxuqhAw1n8h2d434kxB7sUfMeuzCZLArJz_KPN1q2LvOOBaguPRdcgi7WnssWBvFvCc/exec';
         
         // Payment Settings (Google Sheets से)
         this.paymentSettings = {
